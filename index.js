86c1e788976fe75a1a8fc661747d84d66ca36c50
raquel <raquelanaroman@gmail.com>
2021-05-17 22:09:57 -0400

    commit tree 519d8ddc95deba85785669cd68cd3b1e5e6e53d4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin(array){
+    const result = array.find( array => array.result === "W");
+    return !!result ? result.year : undefined;
+}
 
+function superbowlWinUpdate(array){
+    let updatedRecord = [{ year: "2021", result: "L"},{ year: "2020", result: "W"}, { year: "2019", result: "N/A"}, ...array];
+    const result = updatedRecord.find( updatedRecord => updatedRecord.result === "W");
+    return !!result ? result.year : undefined;
+}
